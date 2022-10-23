8385cdc1dad7b498d2c32b935cda78cbb40c502d
sam <contact@justsam.io>
2022-10-23 17:13:49 +0200

    feat(events): add events support

---
+++ event.go
@@ -0,0 +1,81 @@
+package fiberEvents
+
+import (
+	"encoding/json"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/utils"
+)
+
+type Event struct {
+	*sync.Mutex `json:"-"`
+
+	RequestID           string `json:"request_id"`
+	HeaderXForwardedFor string `json:"header_x_forwarded_for"`
+	HeaderReferer       string `json:"header_referer"`
+	HeaderUserAgent     string `json:"header_user_agent"`
+	Path                string `json:"path"`
+	Protocol            string `json:"protocol"`
+	Status              int    `json:"status"`
+	Method              string `json:"method"`
+	Route               string `json:"route"`
+	BytesReceived       int    `json:"bytes_received"`
+	BytesResponded      int    `json:"bytes_responded"`
+	StartTime           string `json:"start_time"`
+	EndTime             string `json:"end_time"`
+	Duration            int64  `json:"duration"`
+
+	Tracelog []*Log                   `json:"tracelog"`
+	Events   []map[string]interface{} `json:"events"`
+}
+
+func (e *Event) AddEvent(name string, value interface{}) {
+	e.Events = append(e.Events, map[string]interface{}{
+		"name":  name,
+		"value": value,
+	})
+}
+
+// Handler - logger will print JSON formatted logs onto STDOUT
+func Handler(ctx *fiber.Ctx) error {
+	start := time.Now()
+
+	rid := string(ctx.Get("X-Request-ID"))
+	if rid == "" {
+		rid = utils.UUID()
+	}
+
+	event := Event{
+		Mutex:               &sync.Mutex{},
+		RequestID:           rid,
+		HeaderXForwardedFor: string(ctx.Get(fiber.HeaderXForwardedFor)),
+		HeaderReferer:       string(ctx.Get(fiber.HeaderReferer)),
+		HeaderUserAgent:     string(ctx.Get(fiber.HeaderUserAgent)),
+		Path:                ctx.Path(),
+		StartTime:           start.String(),
+		Method:              string(ctx.Method()),
+		Route:               string(ctx.Route().Path),
+		BytesReceived:       ctx.Request().Header.ContentLength(),
+		BytesResponded:      ctx.Response().Header.ContentLength(),
+		Protocol:            string(ctx.Protocol()),
+	}
+
+	ctx.Locals("event", &event)
+	ctx.Next()
+
+	event.Status = ctx.Response().StatusCode()
+	event.EndTime = time.Now().String()
+	event.Duration = time.Since(start).Milliseconds()
+
+	logStr, err := json.Marshal(event)
+	log.Printf("%s", string(logStr))
+
+	return err
+}
+
+func New(config ...fiber.Config) fiber.Handler {
+	return Handler
+}
--- log.go
+++ log.go
@@ -1,63 +1,55 @@
-package gofiberJsonLog
+package fiberEvents
 
-import (
-	"encoding/json"
-	"log"
-	"time"
+import "github.com/gofiber/fiber/v2"
 
-	"github.com/gofiber/fiber/v2"
-)
-
-type Record struct {
-	IP                  string `json:"ip"`
-	HeaderXForwardedFor string `json:"header_x_forwarded_for"`
-	Port                string `json:"port"`
-	Path                string `json:"path"`
-	URL                 string `json:"url"`
-	Agent               string `json:"agent"`
-	HeaderReferer       string `json:"header_referer"`
-	Protocol            string `json:"protocol"`
-	Status              int    `json:"status"`
-	Method              string `json:"method"`
-	Route               string `json:"route"`
-	BytesReceived       int    `json:"bytes_received"`
-	BytesResponded      int    `json:"bytes_responded"`
-	StartTime           string `json:"start_time"`
-	EndTime             string `json:"end_time"`
-	Duration            int64  `json:"duration"`
+type Log struct {
+	Level   string `json:"level"`
+	Message string `json:"message"`
 }
 
-// Log - logger will print JSON formatted logs onto STDOUT
-func Log(ctx *fiber.Ctx) error {
-	start := time.Now()
-	logger := Record{
-		IP:                  ctx.IP(),
-		HeaderXForwardedFor: string(ctx.Get(fiber.HeaderXForwardedFor)),
-		Port:                ctx.Port(),
-		Path:                ctx.Path(),
-		URL:                 ctx.OriginalURL(),
-		StartTime:           start.String(),
-		Method:              string(ctx.Method()),
-		Route:               string(ctx.Route().Path),
-		BytesReceived:       ctx.Request().Header.ContentLength(),
-		BytesResponded:      ctx.Response().Header.ContentLength(),
-		Agent:               string(ctx.Get(fiber.HeaderUserAgent)),
-		HeaderReferer:       string(ctx.Get(fiber.HeaderReferer)),
-		Protocol:            string(ctx.Protocol()),
+func eventLogAppend(c *fiber.Ctx, level string, message string) {
+	event := c.Locals("event").(*Event)
+
+	log := &Log{
+		Level:   level,
+		Message: message,
 	}
 
-	ctx.Next()
-
-	logger.Status = ctx.Response().StatusCode()
-	logger.EndTime = time.Now().String()
-	logger.Duration = time.Since(start).Milliseconds()
-
-	logStr, _ := json.Marshal(logger)
-	log.Printf("%s", string(logStr))
-
-	return nil
+	event.Tracelog = append(event.Tracelog, log)
 }
 
-func New(config ...fiber.Config) fiber.Handler {
-	return Log
+func Emergency(c *fiber.Ctx, message string) {
+	eventLogAppend(c, "emergency", message)
+}
+
+func Alert(c *fiber.Ctx, message string) {
+	eventLogAppend(c, "alert", message)
+}
+
+func Critical(c *fiber.Ctx, message string) {
+	eventLogAppend(c, "critical", message)
+}
+
+func Error(c *fiber.Ctx, message string) {
+	eventLogAppend(c, "error", message)
+}
+
+func Warning(c *fiber.Ctx, message string) {
+	eventLogAppend(c, "warning", message)
+}
+
+func Notice(c *fiber.Ctx, message string) {
+	eventLogAppend(c, "notice", message)
+}
+
+func Info(c *fiber.Ctx, message string) {
+	eventLogAppend(c, "info", message)
+}
+
+func Trace(c *fiber.Ctx, message string) {
+	eventLogAppend(c, "trace", message)
+}
+
+func Debug(c *fiber.Ctx, message string) {
+	eventLogAppend(c, "debug", message)
 }
